f3b49d5faf166924374a565f21aeb1c78430197c
ecpelt <ecpelt@utu.fi>
2022-11-13 13:11:58 +0200

    Tehtävä 1 tehty

--- src/main/java/fi/utu/tech/assignment1/Client.java
+++ src/main/java/fi/utu/tech/assignment1/Client.java
@@ -1,10 +1,28 @@
 package fi.utu.tech.assignment1;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+/*
+* Toteuta Client-luokkaan asiakasohjelma,
+* joka pystyy yhdistämään Server-luokan palvelimeen.
+* Voit yhdistäessä käyttää takaisinkytkentäosoitetta
+* ("loopback", 127.0.0.1).
+*/
+
 public class Client {
-
+    // avaa takaisinkytkentäosoitteeseen
     public static void main(String[] args) {
-        // TODO: Asiakasohjelma
-
+        try (Socket s = new Socket(InetAddress.getByName(null), 12346)){
+            if (s.isConnected()) {
+                System.out.println("Yhdistetty!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Suljetaan!");
     }
 
 }
--- src/main/java/fi/utu/tech/assignment1/Server.java
+++ src/main/java/fi/utu/tech/assignment1/Server.java
@@ -1,9 +1,30 @@
 package fi.utu.tech.assignment1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+* Kirjoita Server-luokkaan TCP-soketteihin pohjautuva
+* palvelinohjelma, joka odottaa asiakkaan yhdistämistä.
+* 
+* Aseta palvelin kuuntelemaan yhteydenottopyyntöjä
+* vapaavalintaisessa TCP-portissa.
+*/
+
 public class Server {
 
     public static void main(String[] args) {
-        // TODO: Palvelinohjelma
+        try (ServerSocket ss = new ServerSocket(12346);
+                Socket s = ss.accept();){
+            if(s.isConnected()) {
+                System.out.println("Yhdistetty!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Suljetaan!");
     }
     
 }
