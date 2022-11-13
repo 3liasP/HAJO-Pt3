140bae8bb6c65e474b66c45aaa5def4aa6488cbf
ecpelt <ecpelt@utu.fi>
2022-11-13 15:06:27 +0200

    Tehtävä 2 tehty

--- src/main/java/fi/utu/tech/assignment2/Client.java
+++ src/main/java/fi/utu/tech/assignment2/Client.java
@@ -1,9 +1,37 @@
 package fi.utu.tech.assignment2;
 
-public class Client {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
+/* 
+* Toteuta toiminnallisuus, jossa asiakkaan yhdistettyä
+* palvelimeen, asiakas lähettää merkkijonon "Hello"
+* (tai muun vastaavan) palvelimelle ja palvelin tulostaa
+* vastaanotetun viestin sisällön terminaaliin. Toteuta
+* toiminnallisuus käyttäen suoraan Socket-olioilta
+* saatavia Input- ja OutputStream-oliota ilman, että 
+* käytät tässä kohtaa vielä muita tietovirtaluokkia apuna.
+*/
+
+public class Client {
+    // avaa takaisinkytkentäosoitteeseen
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+        try (Socket s = new Socket(InetAddress.getByName(null), 12346);
+            OutputStream outputStream = s.getOutputStream()) {
+            if (s.isConnected()) {
+                System.out.println("Yhdistetty!");
+            }
+            String greeting = "Terve, palvelin!";
+            outputStream.write(greeting.getBytes());
+            outputStream.flush();
+            System.out.println("Lähetetty!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Suljetaan!");
     }
 
 }
--- src/main/java/fi/utu/tech/assignment2/Server.java
+++ src/main/java/fi/utu/tech/assignment2/Server.java
@@ -1,9 +1,37 @@
 package fi.utu.tech.assignment2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+* Toteuta toiminnallisuus, jossa asiakkaan yhdistettyä
+* palvelimeen, asiakas lähettää merkkijonon "Hello"
+* (tai muun vastaavan) palvelimelle ja palvelin tulostaa
+* vastaanotetun viestin sisällön terminaaliin. Toteuta
+* toiminnallisuus käyttäen suoraan Socket-olioilta
+* saatavia Input- ja OutputStream-oliota ilman, että 
+* käytät tässä kohtaa vielä muita tietovirtaluokkia apuna.
+* 
+*/
+
 public class Server {
 
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+        try (ServerSocket ss = new ServerSocket(12346);
+                Socket s = ss.accept();
+                InputStream inputStream = s.getInputStream()) {
+            if(s.isConnected()) {
+                System.out.println("Yhdistetty!");
+            }
+            String greeting = new String(inputStream.readAllBytes());
+            System.out.println(greeting);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Suljetaan!");
     }
-
+    
 }
