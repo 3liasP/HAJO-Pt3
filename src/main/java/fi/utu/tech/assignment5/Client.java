ad344f145925b7f7fd85257c3ca0170b368c94bd
ecpelt <ecpelt@utu.fi>
2022-11-15 14:32:13 +0200

    Tehtävä 5 tehty

--- src/main/java/fi/utu/tech/assignment5/Client.java
+++ src/main/java/fi/utu/tech/assignment5/Client.java
@@ -1,10 +1,48 @@
 package fi.utu.tech.assignment5;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Random;
+
+/* Asiakkaan rooli on toimia lähinnä testausapuna.
+ * Laita asiakas lähettämään eri komentoja (vaikkapa kovakoodaamalla
+ * lähetettäviä merkkijonoja) ja testaa siten palvelimen toiminta.
+ */
 public class Client {
-
+    // avaa takaisinkytkentäosoitteeseen
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
-
+        try (Socket s = new Socket(InetAddress.getByName(null), 12346);
+            PrintWriter w = new PrintWriter(s.getOutputStream(), true)){
+            Random r = new Random();
+            if (s.isConnected()) {
+                System.out.println("Yhdistetty!");
+            }
+            while (true) {
+                String cmd = "LIGHT;";
+                switch (r.nextInt(3)) {
+                case 0: // ON
+                    cmd = cmd.concat("ON;" + Integer.toString(r.nextInt(5)));
+                    break;
+                case 1: // OFF
+                    cmd = cmd.concat("OFF;" + Integer.toString(r.nextInt(5)));
+                    break;
+                case 2: // QUERY
+                    cmd = cmd.concat("QUERY");
+                }
+                w.println(cmd);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // tyhjä
+                }
+            }  
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Suljetaan!");
     }
 
 }
--- src/main/java/fi/utu/tech/assignment5/ClientHandler.java
+++ src/main/java/fi/utu/tech/assignment5/ClientHandler.java
@@ -1,5 +1,56 @@
 package fi.utu.tech.assignment5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/*
+ * Muuta palvelimen asiakaspalvelijoiden koodia siten, että ne tunnistavat viestit,
+ * jotka noudattavat muotoa "LIGHT;KOMENTO;[ID]<rivinvaihto>", jossa hakasulkeet ID
+ * ympärillä tarkoittavat, että ID on vapaaehtoinen komennosta riippuen (viestissä ei
+ * siis hakasulkeita tarvitse lähettää). Komentovaihtoehdot ovat ON, OFF ja QUERY.
+ * Vastaanottaessa rivinvaihdon, palvelin tietää, että viesti on vastaanotettu ja
+ * voidaan parsia. Viestit voisivat näyttää esimerkiksi seuraavilta:
+ * 
+ * LIGHT;ON;3
+ * LIGHT;OFF;4
+ * LIGHT;QUERY
+ * 
+ * Tässä kohtaa tehtävää riittää, että tunnistaessaan komennon, palvelin tulostaa
+ * konsoliin esimerkiksi "Kytketään lamppu 5 POIS", "Kyselykomento vastaanotettu" jne.
+ */
+
 public class ClientHandler extends Thread {
-    // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+
+    private Socket s;
+
+    public ClientHandler(Socket s) {
+        this.s = s;
+    }
+
+    public void run() {
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter w = new PrintWriter(s.getOutputStream(), true)) {
+                if (s.isConnected()) {
+                    System.out.printf("%h Yhdistetty! %n", this);
+                }
+                while(true) {
+                    String t = r.readLine();
+					String[] cmd = t.split(";", 3);
+                    if (cmd[0].equals("LIGHT") && cmd[1].equals("ON")) {
+                       System.out.printf("%h Kytketään lamppu %s PÄÄLLE%n", this, cmd[2]);
+                    } else if (cmd[0].equals("LIGHT") && cmd[1].equals("OFF")) {
+                        System.out.printf("%h Kytketään lamppu %s POIS%n", this, cmd[2]);
+                    } else if (cmd[0].equals("LIGHT") && cmd[1].equals("QUERY")) {
+                        System.out.printf("%h Kyselykomento vastaanotettu%n", this); 
+                    }
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.printf("%h Suljetaan! %n", this);
+    }
 }
